23ebc5c9ba64130528ef94883ffab644c9722224
FavorKim <harangs99@daum.net>
2024-05-16 15:11:29 +0900

    [Update] Obstacles 스크립트 편의성 개선
    자동으로 플레이어를 대상으로 지정

--- Assets/FavorKim/Scripts/FXs/Obstacles.cs
+++ Assets/FavorKim/Scripts/FXs/Obstacles.cs
@@ -5,6 +5,16 @@ using UnityEngine;
 public class Obstacles : MonoBehaviour
 {
     [SerializeField] int damage;
+    ParticleSystem ps;
+
+    private void Awake()
+    {
+        ps = GetComponent<ParticleSystem>();
+        ParticleSystem.TriggerModule triggerModule = ps.trigger;
+
+        triggerModule.enabled = true;
+        triggerModule.SetCollider(0, FindAnyObjectByType<PlayerController>());
+    }
 
     private void OnParticleTrigger()
     {
