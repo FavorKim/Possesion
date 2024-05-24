2c888e5f0fdf4c3062adc731687254b1c268852e
IamfkingNOOB <iamfkingnoob@gmail.com>
2024-05-24 10:01:03 +0900

    【Update】 싱글톤 제네릭 클래스 생성

--- Assets/FavorKim/Scripts/Utill/Singleton.cs
+++ Assets/FavorKim/Scripts/Utill/Singleton.cs
@@ -1,25 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 {
     private static T instance;
+
     public static T Instance 
     {
         get 
         {
             if (instance == null)
             {
-                instance = FindAnyObjectByType<T>();
+                instance = FindObjectOfType<T>();
+
                 if (instance == null)
                 {
                     GameObject obj = new GameObject();
-                    instance = obj.AddComponent<T>();
-                    DontDestroyOnLoad(obj);
+                    instance = obj.GetComponent<T>();
                 }
             }
+
             return instance; 
+        }
+    }
+
+    private void Awake()
+    {
+        if (transform.parent != null && transform.root != null)
+        {
+            DontDestroyOnLoad(transform.root.gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
         }
     }
 }
