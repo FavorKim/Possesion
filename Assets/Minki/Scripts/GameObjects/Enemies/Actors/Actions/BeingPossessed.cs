a8e8444b0a63e58335691659c1722674476e2ec4
IamfkingNOOB <iamfkingnoob@gmail.com>
2024-05-24 18:44:53 +0900

    【Update】 몬스터가 빙의 상태일 때, 움직임에 따른 애니메이션 변화를 적용
    이걸 왜 이제

--- Assets/Minki/Scripts/GameObjects/Enemies/Actors/Actions/BeingPossessed.cs
+++ Assets/Minki/Scripts/GameObjects/Enemies/Actors/Actions/BeingPossessed.cs
@@ -10,12 +10,16 @@ namespace Enemy
         // 필드(Field)
         private readonly Animator _animator;
         private readonly NavMeshAgent _navMeshAgent;
+        private readonly PlayerController _player;
 
         // 생성자
         public BeingPossessed(Enemy enemy)
         {
             _animator = enemy.GetComponent<Animator>();
             _navMeshAgent = enemy.GetComponent<NavMeshAgent>();
+
+            // 플레이어는 생성자의 호출 시점에서 Find 함수를 사용하여 찾는다.
+            _player = Object.FindObjectOfType<PlayerController>();
         }
 
         // 평가 함수
@@ -33,6 +37,7 @@ namespace Enemy
         {
             // 모든 애니메이션을 초기화하고, 빙의에 대응하는 애니메이션을 활성화한다.
             _animator.SetBool("IsPossessed", true);
+            _animator.SetFloat("Player_Locomotion", _player.Dir.magnitude);
 
             _animator.SetBool("AI_Patrol_Move", false);
             _animator.SetBool("AI_Patrol_Sense", false);
