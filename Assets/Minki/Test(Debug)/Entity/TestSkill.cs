5cffdee20015d9300244695a1d003a2dcdfd3e35
IamfkingNOOB <iamfkingnoob@gmail.com>
2024-06-03 17:03:28 +0900

    【Update】 스킬 클래스 업데이트
    쿨타임 관련 코루틴 함수를 추가합니다.

--- Assets/Minki/Test(Debug)/Entity/TestSkill.cs
+++ Assets/Minki/Test(Debug)/Entity/TestSkill.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
@@ -50,6 +51,9 @@ public class SkillManager
 
 public class Skill
 {
+    // Entity; 코루틴을 실행시키기 위한 변수
+    private Entity entity;
+
     // 스킬의 내용
     private UnityAction skillAction;
 
@@ -57,20 +61,42 @@ public class Skill
     private float maxCoolTime;
     private float curCoolTime = 0;
 
-    public Skill(UnityAction skillAction, float maxCoolTime)
+    // 생성자
+    public Skill(Entity entity, UnityAction skillAction, float maxCoolTime)
     {
+        this.entity = entity;
         this.skillAction = skillAction;
         this.maxCoolTime = maxCoolTime;
     }
 
+    // 스킬을 사용하는 함수
     public void UseSkill()
     {
+        // 현재 재사용 대기 중인지 판별한다.
         bool isCoolTime = (curCoolTime > 0) ? true : false;
 
+        // 지금 바로 사용할 수 있다면,
         if (!isCoolTime)
         {
+            // 스킬 내용을 호출하고, 재사용 대기를 시작한다.
             skillAction.Invoke();
             curCoolTime = maxCoolTime;
+
+            // 관련 코루틴을 실행한다.
+            entity.StartCoroutine(CoolDown());
+        }
+    }
+
+    // 재사용 대기 시간을 감소시키는 코루틴 함수
+    private IEnumerator CoolDown()
+    {
+        // 남은 재사용 대기 시간이 0 이하일 때까지 반복한다.
+        while (curCoolTime >= 0)
+        {
+            // 시간에 비례하여 재사용 대기 시간이 감소한다.
+            curCoolTime -= Time.deltaTime;
+
+            yield return null;
         }
     }
 }
